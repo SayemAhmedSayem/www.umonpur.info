578e89607634443cd74fa532afcdf4acb0e1199e
SayemAhmedSayem <sayemahmed92@gmail.com>
2023-01-26 22:44:49 +0600

    Income and expense  Bangla text update

--- resources/views/backend/expense/index.blade.php
+++ resources/views/backend/expense/index.blade.php
@@ -123,8 +123,8 @@
 
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">বন্ধ করুন</button>
+        <button type="submit" class="btn btn-primary"> যুক্ত করুন</button>
       </div>
     </div>
     </form>
