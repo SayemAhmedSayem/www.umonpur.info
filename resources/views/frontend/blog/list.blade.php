46e25a013e02e948a6b24854dbe12d8cb5b760d0
SayemAhmedSayem <sayemahmed92@gmail.com>
2024-03-16 15:02:44 +0600

    Static user type changed
    View All news  Static user type changed

--- resources/views/frontend/blog/list.blade.php
+++ resources/views/frontend/blog/list.blade.php
@@ -33,7 +33,7 @@
                 </div>
                 <div class="post-content">
                   <h4><i class="fa-solid fa-user"></i>{{$post->user->name ?? ''}}</h4>
-                  <small class="text-muted"><i class="fa-solid fa-tag"></i>{{$post->user->role->name ?? 'অ্যাডমিন'}}</small>
+                  <small class="text-muted"><i class="fa-solid fa-tag"></i>{{$post->user->role->name ?? 'এডমিন'}}</small>
                 </div>
               </div>
               <h5 class="card-title">{{$post->title}}</h5>
