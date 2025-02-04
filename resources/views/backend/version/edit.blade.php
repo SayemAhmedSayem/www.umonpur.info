548cfdd385bea1263f03966a408df4a1babaf4df
SayemAhmedSayem <sayemahmed92@gmail.com>
2025-02-05 04:56:30 +0600

    Update backend version edit.blade.php

--- resources/views/backend/version/edit.blade.php
+++ resources/views/backend/version/edit.blade.php
@@ -20,7 +20,7 @@
     <div class="form-group">
        <div class="row">
           <div class="col-sm-6">
-             <label class="form-label" for="date">ভার্সনের বছর </label>
+             <label class="form-label" for="date">ভার্সন নাম </label>
              <input class="form-control"  name="year" value="{{$version->year}}" type="text">
           </div>
           <div class="col-sm-6">
@@ -29,19 +29,19 @@
           </div>
 
           <div class="col-sm-12">
-             <label class="form-label" for="date">ভার্সন শিরোনাম </label>
+             <label class="form-label" for="date">ভার্সন টপ শিরোনাম </label>
              <input class="form-control" type="text" value="{{$version->version_title}}" name="version_title">
           </div>
           <div class="col-sm-12">
-             <label class="form-label" for="date">ভার্সন শিরোনাম ২ </label>
+             <label class="form-label" for="date">ভার্সন টপ সংকিপ্ত বিবরণ </label>
              <input class="form-control" type="text" value="{{$version->version_subtitle}}" name="version_subtitle">
           </div>
           <div class="col-sm-6">
-             <label class="form-label" for="date">ভার্সন শিরোনাম ৩ </label>
+             <label class="form-label" for="date">ভার্সন শিরোনাম </label>
              <input class="form-control" type="text" value="{{$version->description_title_p1}}" name="description_title_p1">
           </div>
           <div class="col-sm-6">
-             <label class="form-label" for="date">ভার্সন শিরোনাম ৪ </label>
+             <label class="form-label" for="date">ভার্সন প্রকাশের সাল</label>
              <input class="form-control" type="text" value="{{$version->description_title_p2}}" name="description_title_p2">
           </div>
        </div>
