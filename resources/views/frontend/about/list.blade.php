5092247ba6536fc5b3201b6b91e3dadc99b30b15
SayemAhmedSayem <sayemahmed92@gmail.com>
2025-02-06 23:16:47 +0600

    Update frontend about list.blade.php

--- resources/views/frontend/about/list.blade.php
+++ resources/views/frontend/about/list.blade.php
@@ -6,9 +6,7 @@
     <div class="container">
       <div class="banner-title">
         <h2><span> সম্পর্কে</span></h2>
-        <!-- <p class="banner-title-desc">গাইবান্ধার বালাসী থেকে জামালপুরের বাহাদুরাবাদ নৌরুটে এক মাস আগে লঞ্চ পারাপারের
-          ব্যবস্থা চালু হয়। বালাসীঘাটে
-          তিনটি লঞ্চ রয়েছে</p> -->
+        <!-- <p class="banner-title-desc">গাইবান্ধার বালাসী থেকে জামালপুরের বাহাদুরাবাদ </p> -->
         <p><a href="index.html">হোম</a> <i class="fa fa-angle-right"></i><a href="about.html">সম্পর্কে</a>
           <i class="fa fa-angle-right"></i> <span>সম্পর্কে_আরো</span></p>
       </div>
@@ -20,10 +18,9 @@
   <section class="">
     <div class="container">
       <div class="section-title">
-        <a href="about_details.html">
         <h2>আমাদের সম্পর্কে</h2>
-        <p>উমনপুর গ্রামের কিছু নির্দিষ্ট অংশের তথ্যের লিস্ট এইখানে দেখতে পাবেন।</p>
-        </a>
+        <p>এই পোর্শনে গ্রামের নির্দিষ্ট বিষয়ের তথ্য লিস্টের সর্বশেষ হগালনাগাদ</p>
+        
       </div>
 
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4 about-post">
@@ -39,7 +36,6 @@
             <p>{!! Str::limit($about->description, 100) !!}</p>
               <a href="{{route('web-about-show', $about->id)}}" class="btn btn-box">বিস্তারিত</a>
             </div>
-
           </div>
         </div>
         @endforeach
