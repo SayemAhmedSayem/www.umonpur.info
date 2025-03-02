815836e9356bd3597b49ae8e6393414f0317d74a
SayemAhmedSayem <sayemahmed92@gmail.com>
2025-03-02 22:58:06 +0600

    Update backend sliders index.blade.php

--- resources/views/backend/sliders/index.blade.php
+++ resources/views/backend/sliders/index.blade.php
@@ -63,7 +63,9 @@
                    
                         </tbody>
                       </table>
-                    
+                      <div class="d-flex">
+                {!! $slider->links() !!}
+            </div>
                     </div>
         </div>
     
